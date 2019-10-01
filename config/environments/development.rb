80f8ca2642e0bb9adece80cb7967ec9aab2c1436
Samantha Silva <samanthasilva.n@gmail.com>
2019-10-01 16:52:28 -0300

    Sign in with facebook

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
