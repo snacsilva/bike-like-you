7e539158d843982b77c3bc7f9472f943b1ae74c9
Samantha Silva <samanthasilva.n@gmail.com>
2019-10-02 10:47:00 -0300

    Figaro env add.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   
+  root to: 'stations#index'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :bikes
   resources :trips
